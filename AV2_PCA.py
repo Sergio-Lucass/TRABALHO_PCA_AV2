e4eaa650ee31c7f90eee4c17bd3583538d83382a
Sergio_Lucas <sergio.rod.guapi@gmail.com>
2025-12-12 00:39:30 -0300

    Criando a função para gerar a matricula

--- AV2_PCA.py
+++ AV2_PCA.py
@@ -5,3 +5,14 @@ nome_arquivo = "inf_alunos.csv"
 COLUNAS_COMPLETAS = ["Matricula", "Nome", "Rua", "Numero", "Bairro", "Cidade", "UF", "Telefone", "Email"]
 CAMPOS_ALUNO = ["Nome", "Rua", "Numero", "Bairro", "Cidade", "UF", "Telefone", "Email"]
 
+def criar_matricula(df):
+  if df.empty:
+    return 1
+  
+  try:
+    max_val = df["Matricula"].max()
+    if pd.isna(max_val): 
+      return 1
+    return int(max_val) + 1
+  except:
+      return len(df) + 1
