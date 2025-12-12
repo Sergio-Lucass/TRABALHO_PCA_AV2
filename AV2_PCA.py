cb9063e6dbed9ee5b0f2a02ca138b28e41ecec2c
Sergio_Lucas <sergio.rod.guapi@gmail.com>
2025-12-12 00:24:49 -0300

    Definindo o nome do arquivo csv e criando os vetores associados ao aluno

--- AV2_PCA.py
+++ AV2_PCA.py
@@ -1,2 +1,7 @@
 import os
 import pandas as pd
+nome_arquivo = "inf_alunos.csv"
+
+COLUNAS_COMPLETAS = ["Matricula", "Nome", "Rua", "Numero", "Bairro", "Cidade", "UF", "Telefone", "Email"]
+CAMPOS_ALUNO = ["Nome", "Rua", "Numero", "Bairro", "Cidade", "UF", "Telefone", "Email"]
+
